6c5a458120b31718db857148b0aba762c4890cd6
Nishitha Ningegowda <nishitha.ningegowda@gmail.com>
2012-09-29 00:11:25 -0500

    Nishi adding todo list skeleton built in rails, bootstrap, mysql

---
+++ app/models/list.rb
@@ -0,0 +1,6 @@
+class List < ActiveRecord::Base
+  attr_accessible :description, :name
+  validates :name, :presence => true
+
+  has_many :tasks
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoList::Application.config.secret_token = '28663bcbcac63437e8a2762e384f5a798408040c4435fdbbf0183cdf0aef71a4bb3ba7273b45b0621e01c1ae34d0f49ec84d3f73c763223b459c1ac52dfb209c'
